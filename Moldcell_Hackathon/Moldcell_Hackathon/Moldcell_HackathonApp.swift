d30fc2d2d97d656039e67c9d323c1397cb40a50f
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-15 13:32:37 +0200

    init commit

---
+++ Moldcell_Hackathon/Moldcell_Hackathon/Moldcell_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moldcell_HackathonApp.swift
+//  Moldcell_Hackathon
+//
+//  Created by Dragomir Mindrescu on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Moldcell_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
