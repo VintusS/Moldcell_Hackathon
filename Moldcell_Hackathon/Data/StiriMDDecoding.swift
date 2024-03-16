113eaa5e0678023f5ea627c917ccebcafbb5bea7
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-16 19:23:39 +0200

    News article example

---
+++ Moldcell_Hackathon/Data/StiriMDDecoding.swift
@@ -0,0 +1,8 @@
+//
+//  StiriMDDecoding.swift
+//  Moldcell_Hackathon
+//
+//  Created by Dragomir Mindrescu on 16.03.2024.
+//
+
+import Foundation
---
+++ Moldcell_Hackathon/News/StiriMDView.swift
@@ -0,0 +1,18 @@
+//
+//  StiriMDView.swift
+//  Moldcell_Hackathon
+//
+//  Created by Dragomir Mindrescu on 16.03.2024.
+//
+
+import SwiftUI
+
+struct StiriMDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StiriMDView()
+}
