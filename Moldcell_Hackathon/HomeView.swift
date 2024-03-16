4585143d7a4c2ecbb13c95400ad1b045d44b9aec
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-16 17:05:10 +0200

    MainPage Dashboard links

--- Moldcell_Hackathon/HomeView.swift
+++ Moldcell_Hackathon/HomeView.swift
@@ -10,41 +10,173 @@ import SwiftUI
 struct HomeView: View {
     
     @AppStorage("customPhoneNumber") var customPhoneNumber: String = ""
+    @State private var timeOfDay: String
+    @State private var feelingText: String
+
+    let sadMessages = [
+            "E ok să fiți trist uneori.",
+            "Suntem aici pentru voi."
+        ]
+        
+        let normalMessages = [
+            "O zi calmă, o zi binecuvântată.",
+            "Totul va fi bine."
+        ]
+        
+        let happyMessages = [
+            "Zâmbetul tău înseninează tot!",
+            "Rază de soare!"
+        ]
+    
+    init() {
+        let currentDayTime = getTimeOfDay()
+        _timeOfDay = State(initialValue: currentDayTime)
+        _feelingText = State(initialValue: "Cum va simtiti in aceasta \(currentDayTime)?")
+    }
     
     var body: some View {
-        VStack {
-            HStack {
+        ScrollView {
+            VStack {
                 HStack {
-                    Image("profilePhoto")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 80, height: 50)
-                        .clipShape(Circle())
+                    HStack {
+                        Image("profilePhoto")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 80, height: 50)
+                            .clipShape(Circle())
+                        
+                        Text("Profile Name") // Replace with the actual profile name
+                            .font(.custom("BalooBhai-Regular", size: 18))
+                    }
                     
-                    Text("Profile Name") // Replace with the actual profile name
-                        .font(.custom("BalooBhai-Regular", size: 18))
+                    Spacer()
+                    
+                    Button(action: {
+                        let phoneNumber = customPhoneNumber
+                        if let url = URL(string: "tel://\(phoneNumber)"), UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url)
+                        }
+                    }) {
+                        Image(systemName: "phone.circle.fill") // Use a phone icon
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.blue) // Color for the button
+                    }
+                    
                 }
+                .padding()
+                
+                HStack{
+                    Text("Panoul Meu")
+                        .font(.custom("BalooBhai-Regular", size: 30))
+                    Spacer()
+                }
+                .padding(.leading, 35)
+                .padding(.bottom, 0)
+                .padding(.top, 0)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .padding([.leading, .trailing], 35)
+                        .padding(.top, -25)
+                        .foregroundStyle(Color.blue)
+                        .shadow(radius: 10)
+                        .frame(height: 200)
+                    
+                    VStack {
+                        Text(feelingText)
+                            .foregroundStyle(Color.white)
+                            .font(.custom("BalooBhai-Regular", size: 25))
+                            .lineLimit(3)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .multilineTextAlignment(.center)
+                            .padding([.leading, .trailing], 36)
+                        
+                        HStack(spacing: 30) {
+                            Button(action: {
+                                feelingText = sadMessages.randomElement() ?? "Aveți grijă de voi!"
+                            }) {
+                                Text("😞")
+                                    .font(.custom("BalooBhai-Regular", size: 75))
+                            }
+                            
+                            Button(action: {
+                                feelingText = normalMessages.randomElement() ?? "O zi liniștită este o binecuvântare."
+                            }) {
+                                Text("😐")
+                                    .font(.custom("BalooBhai-Regular", size: 75))
+                            }
+                            
+                            Button(action: {
+                                feelingText = happyMessages.randomElement() ?? "Zâmbetul vostru ne încălzește inimile!"
+                            }) {
+                                Text("😊")
+                                    .font(.custom("BalooBhai-Regular", size: 75))
+                            }
+                        }
+                    }
+                }
+                
+                HStack(spacing: 20) {
+                    VStack {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.white)
+                        Text("Galerie")
+                            .foregroundColor(.white)
+                            .font(.custom("BalooBhai-Regular", size: 18))
+                    }
+                    .frame(width: 150, height: 150)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                    .shadow(radius: 10)
+
+                    VStack {
+                        Image(systemName: "message")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.white)
+                        Text("Conversații")
+                            .foregroundColor(.white)
+                            .font(.custom("BalooBhai-Regular", size: 18))
+                    }
+                    .frame(width: 150, height: 150)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                    .shadow(radius: 10)
+                }
+                .padding([.leading, .trailing], 35)
+                .padding(.top, -65)
+        
+                HStack{
+                    Text("Noutati")
+                        .font(.custom("BalooBhai-Regular", size: 30))
+                    Spacer()
+                }
+                .padding(.leading, 35)
+                .padding(.bottom, 0)
+                .padding(.top, 0)
                 
                 Spacer()
-                
-                Button(action: {
-                    let phoneNumber = customPhoneNumber
-                    if let url = URL(string: "tel://\(phoneNumber)"), UIApplication.shared.canOpenURL(url) {
-                        UIApplication.shared.open(url)
-                    }
-                }) {
-                    Image(systemName: "phone.circle.fill") // Use a phone icon
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.blue) // Color for the button
-                }
             }
-            .padding()
-            
-            
-            
-            Spacer()
         }
+    }
+}
+
+func getTimeOfDay() -> String {
+    let hour = Calendar.current.component(.hour, from: Date())
+    
+    switch hour {
+    case 6..<12:
+        return "dimineata"
+    case 12..<17:
+        return "zi"
+    case 17..<21:
+        return "seara"
+    default:
+        return "noapte"
     }
 }
 
--- Moldcell_Hackathon/Moldcell_HackathonApp.swift
+++ Moldcell_Hackathon/Moldcell_HackathonApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Moldcell_HackathonApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginPage()
+            MainPage()
         }
     }
 }
