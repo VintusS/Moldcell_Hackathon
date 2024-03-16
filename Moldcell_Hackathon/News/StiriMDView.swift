1186ec9a6e64d5cd2a2df30a59e2a946edf2e4b5
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-16 21:13:03 +0200

    all the articles in one scrollable list

--- Moldcell_Hackathon/News/StiriMDView.swift
+++ Moldcell_Hackathon/News/StiriMDView.swift
@@ -6,20 +6,50 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct StiriMDView: View {
     let articles = loadStiriMDArticles()
+    private let speechSynthesizer = AVSpeechSynthesizer()
+    @State private var isSpeaking = true
 
     var body: some View {
         List(articles, id: \.Article) { article in
             VStack(alignment: .leading) {
                 Text(article.Title)
-                    .font(.headline)
+                    .font(.custom("BalooBhai-regular", size: 20))
+                
+                HStack {
+                    Button(isSpeaking ? "Incepe sa citesti" : "Opreste citirea") {
+                        if isSpeaking {
+                            speakArticle(article)
+                        } else {
+                            speechSynthesizer.stopSpeaking(at: .immediate)
+                        }
+                        isSpeaking.toggle()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .font(.custom("BalooBhai-regular", size: 15))
+                    .foregroundStyle(.blue)
+                    
+                    Spacer()
+                    
+                    Button("Acceseaza site-ul") {
+                        if let url = URL(string: article.Article), UIApplication.shared.canOpenURL(url) {
+                            DispatchQueue.main.async {
+                                UIApplication.shared.open(url)
+                            }
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .font(.custom("BalooBhai-regular", size: 15))
+                    .foregroundStyle(.blue)
+                }
                 
                 Divider()
                 
                 Text(article.Subtitle)
-                    .font(.subheadline)
+                    .font(.custom("BalooBhai-regular", size: 15))
                     .padding(.bottom)
                 
                 if let imageURL = URL(string: article.Image) {
@@ -28,16 +58,23 @@ struct StiriMDView: View {
                     } placeholder: {
                         ProgressView()
                     }
-                    .aspectRatio(contentMode: .fill)
                     .frame(height: 200)
                     .cornerRadius(10)
                     .padding(.bottom)
                 }
+                
                 Text(article.Description)
-                    .font(.subheadline)
-                    .padding(.bottom)
+                    .font(.custom("BalooBhai-regular", size: 15))
             }
         }
+        .scrollContentBackground(.hidden)
+    }
+    
+    private func speakArticle(_ article: NewsArticle) {
+        let textToSpeak = "\(article.Title). \(article.Subtitle). \(article.Description)"
+        let utterance = AVSpeechUtterance(string: textToSpeak)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ro-RO")
+        speechSynthesizer.speak(utterance)
     }
 }
 
