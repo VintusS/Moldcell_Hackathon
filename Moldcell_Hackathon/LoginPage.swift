86e7ae5c7ddd22287a1390c7a0298dc07b0cd4d8
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-16 11:37:16 +0200

    Registration Page Design

--- Moldcell_Hackathon/LoginPage.swift
+++ Moldcell_Hackathon/LoginPage.swift
@@ -10,75 +10,85 @@ import SwiftUI
 struct LoginPage: View {
     @State private var phoneNumber: String = ""
     @State private var password: String = ""
+    @State private var showValidationErrors: Bool = false
 
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
-            Spacer()
-            Image("icon")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 100)
-                .cornerRadius(90)
-
-            Text("SilverLink")
-                .font(.custom("BalooBhai-Regular", size: 40))                .fontWeight(.bold)
-
-            ZStack {
-                RoundedRectangle(cornerRadius: 90)
-                    .frame(width: 300, height: 60)
-                    .foregroundColor(.white)
-                    .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
-
-                TextField("Numar de Telefon", text: $phoneNumber)
-                    .font(.custom("BalooBhai-Regular", size: 15))
-                    .foregroundColor(Color.black.opacity(0.5))
-                    .frame(width: 280, height: 60)
-                    .padding(.leading, 20)
-            }
-            
-            ZStack {
-                RoundedRectangle(cornerRadius: 90)
-                    .frame(width: 300, height: 60)
-                    .foregroundColor(.white)
-                    .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
-
-                SecureField("Parola", text: $password)
-                    .font(.custom("BalooBhai-Regular", size: 15))
-                    .foregroundColor(Color.black.opacity(0.5))
-                    .frame(width: 280, height: 60)
-                    .padding(.leading, 20)
-            }
-            .padding()
-            Button("CONECTEAZA-TE") {
-                // Handle the sign-in action
-            }
+        NavigationStack {
+            VStack(alignment: .center, spacing: 20) {
+                Spacer()
+                Image("icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(90)
+                
+                Text("SilverLink")
+                    .font(.custom("BalooBhai-Regular", size: 40))                .fontWeight(.bold)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 90)
+                        .frame(width: 300, height: 60)
+                        .foregroundColor(.white)
+                        .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
+                    
+                    TextField("Numar de Telefon", text: $phoneNumber)
+                        .font(.custom("BalooBhai-Regular", size: 15))
+                        .foregroundColor(Color.black.opacity(0.5))
+                        .frame(width: 280, height: 60)
+                        .padding(.leading, 20)
+                        .background(phoneNumber.isEmpty && showValidationErrors ? Color.red.opacity(0.1) : Color.white)
+                        .cornerRadius(90)
+                }
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 90)
+                        .frame(width: 300, height: 60)
+                        .foregroundColor(.white)
+                        .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
+                    
+                    SecureField("Parola", text: $password)
+                        .font(.custom("BalooBhai-Regular", size: 15))
+                        .foregroundColor(Color.black.opacity(0.5))
+                        .frame(width: 280, height: 60)
+                        .padding(.leading, 20)
+                        .background(password.isEmpty && showValidationErrors ? Color.red.opacity(0.1) : Color.white)
+                        .cornerRadius(90)
+                }
+                .padding()
+                Button("CONECTEAZA-TE") {
+                    // Validate input fields
+                    showValidationErrors = phoneNumber.isEmpty || password.isEmpty
+                    if !showValidationErrors {
+                        // Proceed with login
+                    }
+                }
                 .font(.custom("BalooBhai-Regular", size: 20))
                 .frame(width: 300, height: 60)
                 .background(Color.blue)
                 .foregroundColor(.white)
                 .cornerRadius(90)
                 .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
-
-            Text("Inca nu ai un cont?")
+                
+                Text("Inca nu ai un cont?")
+                    .font(.custom("BalooBhai-Regular", size: 15))
+                    .foregroundColor(Color.black.opacity(0.5))
+                NavigationLink(destination: RegistrationPage()) {
+                    Text("INREGISTREAZA-TE ACUM")
+                        .font(.custom("BalooBhai-Regular", size: 20))
+                }
+                
+                Spacer()
+                // "Terms and conditions" link
+                Button("Termeni si Conditii") {
+                    // Handle the link action
+                }
                 .font(.custom("BalooBhai-Regular", size: 15))
-                .foregroundColor(Color.black.opacity(0.5))
-            
-            Button("INREGISTREAZA-TE ACUM") {
-                // Handle the sign-up action
+                .foregroundStyle(Color.black.opacity(0.5))
             }
-            .font(.custom("BalooBhai-Regular", size: 20))
-            
-            Spacer()
-            // "Terms and conditions" link
-            Button("Termeni si Conditii") {
-                // Handle the link action
-            }
-            .font(.custom("BalooBhai-Regular", size: 15))
-            .foregroundStyle(Color.black.opacity(0.5))
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(Color(hex: "F7F7F7"))
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        .background(Color(hex: "F7F7F7"))
     }
 }
 
@@ -95,16 +105,6 @@ extension Color {
         let b = Double(rgbValue & 0x0000FF) / 255.0
 
         self.init(red: r, green: g, blue: b)
-    }
-}
-
-struct FilledRoundedButtonStyle: ButtonStyle {
-    func makeBody(configuration: Self.Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
--- Moldcell_Hackathon/RegistrationPage.swift
+++ Moldcell_Hackathon/RegistrationPage.swift
@@ -8,8 +8,94 @@
 import SwiftUI
 
 struct RegistrationPage: View {
+    @State private var name: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var confirmPhoneNumber: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    @State private var isAgreedToTerms = false
+    @State private var showValidationErrors: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 20) {
+            Spacer()
+
+            Text("SilverLink")
+                .font(.custom("BalooBhai-Regular", size: 40))
+                .fontWeight(.bold)
+            
+            Text("Completati formularul de inregistrare")
+                .font(.custom("BalooBhai-Regular", size: 15))
+                .fontWeight(.regular)
+
+            Group {
+                customTextField(placeholder: "Nume", text: $name, isFieldValid: !name.isEmpty)
+                customTextField(placeholder: "Numar de Telefon", text: $phoneNumber, isFieldValid: !phoneNumber.isEmpty)
+                customTextField(placeholder: "Confirma Numarul de Telefon", text: $confirmPhoneNumber, isFieldValid: phoneNumber == confirmPhoneNumber && !confirmPhoneNumber.isEmpty)
+                customSecureField(placeholder: "Parola", text: $password, isFieldValid: !password.isEmpty)
+                customSecureField(placeholder: "Confirma Parola", text: $confirmPassword, isFieldValid: password == confirmPassword && !confirmPassword.isEmpty)
+            }
+
+            HStack {
+                Button(action: {
+                    isAgreedToTerms.toggle()
+                }) {
+                    Image(systemName: isAgreedToTerms ? "checkmark.square.fill" : "square")
+                        .foregroundColor(isAgreedToTerms ? .blue : .gray)
+                    Text("Sunt de acord cu")
+                        .font(.custom("BalooBhai-Regular", size: 15))
+                        .foregroundColor(Color.black.opacity(0.5))
+                }
+
+                NavigationLink(destination: Text("Terms and Conditions page")) {
+                    Text("Termenii si Conditiile")
+                        .font(.custom("BalooBhai-Regular", size: 15))
+                        .foregroundColor(Color.blue)
+                        .underline()
+                }
+            }
+            .frame(width: 300, height: 60)
+
+            Button("INREGISTREAZA-TE ACUM") {
+                showValidationErrors = name.isEmpty || phoneNumber.isEmpty || confirmPhoneNumber.isEmpty || password.isEmpty || confirmPassword.isEmpty
+                if !showValidationErrors {
+                    // Proceed with login
+                }
+            }
+            .font(.custom("BalooBhai-Regular", size: 20))
+            .frame(width: 300, height: 60)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(90)
+            .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
+
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .background(Color(hex: "F7F7F7"))
+    }
+
+    private func customTextField(placeholder: String, text: Binding<String>, isFieldValid: Bool) -> some View {
+        TextField(placeholder, text: text)
+            .font(.custom("BalooBhai-Regular", size: 15))
+            .foregroundColor(Color.black.opacity(0.5))
+            .frame(width: 300, height: 60)
+            .padding(.leading, 20)
+            .background(phoneNumber.isEmpty && showValidationErrors ? Color.red.opacity(0.1) : Color.white)
+            .cornerRadius(90)
+            .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
+    }
+
+    private func customSecureField(placeholder: String, text: Binding<String>, isFieldValid: Bool) -> some View {
+        SecureField(placeholder, text: text)
+            .font(.custom("BalooBhai-Regular", size: 15))
+            .foregroundColor(Color.black.opacity(0.5))
+            .frame(width: 300, height: 60)
+            .padding(.leading, 20)
+            .background(phoneNumber.isEmpty && showValidationErrors ? Color.red.opacity(0.1) : Color.white)
+            .cornerRadius(90)
+            .shadow(color: Color.black.opacity(0.12), radius: 10, x: 0, y: 3)
     }
 }
 
