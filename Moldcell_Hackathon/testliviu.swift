9fc2b666c069c01acbd2b6f5f2171169b3f85d4e
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-16 15:24:46 +0200

    login page fix. redirection to main page. screen position set only for portrait

---
+++ Moldcell_Hackathon/testliviu.swift
@@ -0,0 +1,18 @@
+//
+//  testliviu.swift
+//  Moldcell_Hackathon
+//
+//  Created by Dragomir Mindrescu on 16.03.2024.
+//
+
+import SwiftUI
+
+struct testliviu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testliviu()
+}
