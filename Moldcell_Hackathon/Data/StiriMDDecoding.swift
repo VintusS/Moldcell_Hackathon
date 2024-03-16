ef17153a0adca5ea5d9ed2a6639a388bd57e497a
VintusS <dragomir.mindrescu@isa.utm.md>
2024-03-16 19:23:59 +0200

    News Article Preview

--- Moldcell_Hackathon/Data/StiriMDDecoding.swift
+++ Moldcell_Hackathon/Data/StiriMDDecoding.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct NewsArticle: Decodable {
+    var Article: String
+    var Image: String
+    var Title: String
+    var Subtitle: String
+    var Description: String
+}
+
+func loadStiriMDArticles() -> [NewsArticle] {
+    guard let url = Bundle.main.url(forResource: "StiriMDArticles", withExtension: "json") else {
+        fatalError("JSON file not found")
+    }
+
+    do {
+        let data = try Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        let articles = try decoder.decode([NewsArticle].self, from: data)
+        return articles
+    } catch {
+        fatalError("Failed to decode JSON: \(error)")
+    }
+}
--- Moldcell_Hackathon/News/StiriMDView.swift
+++ Moldcell_Hackathon/News/StiriMDView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct StiriMDView: View {
+    let articles = loadStiriMDArticles()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(articles, id: \.Article) { article in
+            VStack(alignment: .leading) {
+                Text(article.Title)
+                    .font(.headline)
+                
+                Divider()
+                
+                Text(article.Subtitle)
+                    .font(.subheadline)
+                    .padding(.bottom)
+                
+                if let imageURL = URL(string: article.Image) {
+                    AsyncImage(url: imageURL) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 200)
+                    .cornerRadius(10)
+                    .padding(.bottom)
+                }
+                Text(article.Description)
+                    .font(.subheadline)
+                    .padding(.bottom)
+            }
+        }
     }
 }
 
